d3bfd3b5b243690ac08d555fa45ea2491e72f7e5
lauren-cardenas <lauren.cardenas@sfdr-cisd.org>
2019-11-05 13:44:39 -0600

    Update DriveTrain.java
    Inserted code for reading encoders

--- subsystems/DriveTrain.java
+++ subsystems/DriveTrain.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import frc.robot.RobotMap;
 
 
@@ -24,6 +25,43 @@ public class DriveTrain extends Subsystem {
   VictorSPX motorRight1 = new VictorSPX(RobotMap.RIGHT_FRONT_MOTOR);
   VictorSPX motorRight2 = new VictorSPX(RobotMap.RIGHT_REAR_MOTOR);
 
+  
+  //Encoders begin to count as soon as the robot is on and the motors move
+  // Digital input ports can be found on RobotMap
+  // False indicates that no inversion in counting direction is happening
+  // k4X is the encoding type
+
+  Encoder leftEnc = new Encoder(RobotMap.leftEnc1, RobotMap.leftEnc2, false, Encoder.EncodingType.k4X);
+  Encoder rightEnc = new Encoder(RobotMap.rightEnc1, RobotMap.rightEnc2, false, Encoder.EncodingType.k4X);
+  
+  //Gets a raw encoder value
+  //Similarly, getCount will give you the number of
+  //"clicks" it has recorded
+
+  public double getLeftRaw(){
+    return leftEnc.getRaw();
+  }
+
+  public double getRightRaw(){
+    return rightEnc.getRaw();
+  }
+
+  //Averages the raw values of the left and right encoders
+  public double getRawAvg(){
+    return (leftEnc.getRaw() + rightEnc.getRaw())/2;
+  }
+
+  //Gets a distance based on a factor for units per encoder value
+  //Units used is up to the team
+  public double getAvgDistance(){
+    return getRawAvg() * 0.0008;
+  }
+
+  //Resets the encoders so that they read from 0 again
+  public void encReset(){
+    leftEnc.reset();
+    rightEnc.reset();
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
